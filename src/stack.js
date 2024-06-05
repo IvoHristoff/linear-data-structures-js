dde8af5eca24b1293ed30886041c1d2501908c0f
Ivo <hristovivo24@gmail.com>
2024-06-05 21:02:38 +0300

    implementing stack

--- src/stack.js
+++ src/stack.js
@@ -1,10 +1,77 @@
 import LinkedListNode from "./linked-list-node.js";
 
-// Stack implementation using linked list
+/**
+ * Stack implementation using linked list.
+ * @class
+ */
 export class Stack {
 
+     /**
+     * @private
+     * @type {LinkedListNode|null}
+     */
+    #top = null;
 
+    /**
+     * @private
+     * @type {number}
+     */
+    #counter = 0;
 
+    /**
+     * Pushes a value onto the stack.
+     * @param {*} value - The value to be pushed onto the stack.
+     */
+    push(value){
+        const node = new LinkedListNode(value);
+        node.next = this.#top;
+        this.#top = node;
+        this.#counter++;
+    }
 
+    /**
+     * Pops a value from the stack.
+     * @returns {*} The value that was popped from the stack.
+     * @throws {Error} If the stack is empty.
+     */
+    pop(){
+        if (!this.#top){
+            throw new Error('Stack is empty')
+        }
+        const value = this.#top;
+        this.#top = this.#top.next
+
+        this.#counter--;
+        return value;
+    }
+
+    /**
+     * Peeks at the top value of the stack without removing it.
+     * @returns {*} The value at the top of the stack.
+     * @throws {Error} If the stack is empty.
+     */
+    peek(){
+        if (!this.#top){
+            throw new Error('Stack is empty')
+        }
+
+        return this.#top.value;
+    }
+
+    /**
+     * Checks if the stack is empty.
+     * @returns {boolean} True if the stack is empty, false otherwise.
+     */
+    get isEmpty(){
+        return !this.#top;
+    }
+
+    /**
+     * Gets the number of elements in the stack.
+     * @returns {number} The number of elements in the stack.
+     */
+    get count(){
+     return this.#counter;   
+    }
 
 }
