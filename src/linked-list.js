3ce2b733a6fada82d5d00e6b263bc5d293ac4704
IvoHristoff <hristovivo24@gmail.com>
2024-06-09 21:37:43 +0300

    implemented linked list

---
+++ src/linked-list.js
@@ -0,0 +1,98 @@
+/**
+ * LinkedList class represents a singly linked list data structure.
+ * @class
+ */
+class LinkedList {
+    /**
+     * @private
+     * @type {ListNode|null}
+     * The head of the linked list.
+     */
+    #head = null;
+
+    /**
+     * @private
+     * @type {number}
+     * The counter to keep track of the number of nodes in the linked list.
+     */
+    #counter = 0;
+
+    /**
+     * Adds a new node with the given value to the beginning of the linked list.
+     * @param {*} value - The value to be added to the list.
+     */
+    addFirst(value){
+        const node = new ListNode(value);
+        node.next = this.#head;
+        this.#head = node;
+        this.#counter++;
+    }
+
+    /**
+     * Removes and returns the value of the first node in the linked list.
+     * @returns {*} The value of the removed node.
+     * @throws Will throw an error if the list is empty.
+     */
+    removeFirst(){
+        if (!this.#head){
+            throw new Error('List is empty, therefore can\'t remove anything.');
+        }
+
+        const value = this.#head.value;
+        this.#head = this.#head.next;
+        this.#counter--;
+
+        return value;
+    }
+
+    /**
+     * Inserts a new node with the given value after the specified node.
+     * @param {ListNode} node - The node after which the new node will be inserted.
+     * @param {*} value - The value to be added to the list.
+     */
+    insertAfter(node, value){
+        const newNode = new ListNode(value);
+        newNode.next = node.next;
+        node.next = newNode;
+        this.#counter++;
+    }
+
+    /**
+     * Removes the node that comes after the specified node.
+     * @param {ListNode} node - The node after which the next node will be removed.
+     */
+    removeAfter(node){
+        if (node.next){
+            node.next = node.next.next;
+            this.#counter--;
+        }
+    }
+
+    /**
+     * Finds the first node with the specified value.
+     * @param {*} value - The value to search for in the list.
+     * @returns {ListNode|null} The node with the specified value, or null if not found.
+     */
+    find(value){
+        let ref = this.#head;
+        while (ref){
+            if (value === ref.value){
+                return ref;
+            }
+            ref = ref.next;
+        }
+        return null;
+    }
+
+    /**
+     * Generator function to iterate over the linked list.
+     * @returns {IterableIterator<*>} An iterator that yields the values of the nodes in the list.
+     */
+    *[Symbol.iterator](){
+        let current = this.#head;
+        while (current) {
+            yield current.value;
+            current = current.next;
+        }
+    }
+}
